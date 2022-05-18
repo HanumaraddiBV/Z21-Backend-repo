d8ba7ad3119d21a5c38ea78fcc159f0ebb9c14ad
Hanumaraddi vaddatti <93374801+HanumaraddiBV@users.noreply.github.com>
2022-05-18 18:29:14 +0530

    updated scripts

--- src/controllers/mutulFund.controller.js
+++ src/controllers/mutulFund.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const Data = require("../models/mutulfundData.model");
-
+/////////
 //get method for all collection
 router.get("/", async (req, res) => {
   try {
